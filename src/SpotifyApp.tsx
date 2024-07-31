345cb73e4b697f00d44b625ad645e8b45cc61a2c
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-07-31 11:28:13 +0200

    navigation install. & config. + project structure folder

---
+++ src/SpotifyApp.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+
+
+export const SpotifyApp = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>SpotifyApp</Text>
+      </View>
+    </NavigationContainer>
+    
+  );
+};
